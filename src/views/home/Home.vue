6235dde815c4e8ad9522989282727ff5d60b6021
fenfenraorao <877956390@qq.com>
2018-12-17 15:55:48 +0800

    feat(home):Add home

---
+++ src/views/home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="page">
+    <div class="home">home</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+.home{
+  font-size: 50px;
+}
+</style>
