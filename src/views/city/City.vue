1d24fa99623c99ce20f29810c1e54cae469603ea
fenfenraorao <877956390@qq.com>
2018-12-18 20:07:02 +0800

    feat(city):Add header

---
+++ src/views/city/City.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="city">
+    <city-header></city-header>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+
+export default {
+  name: 'city',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
