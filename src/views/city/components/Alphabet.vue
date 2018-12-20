be6597fe8e693b2c31fad46fd6fab8144f09de9c
fenfenraorao <877956390@qq.com>
2018-12-20 18:26:54 +0800

    feat(city):Add list and alphabet

---
+++ src/views/city/components/Alphabet.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="list">
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.list
+  display flex
+  flex-direction column
+  justify-content center
+  position absolute
+  right 0
+  top $headerHeight + 0.72rem
+  bottom 0
+  width 0.4rem
+  .item
+    line-height 0.4rem
+    text-align center
+    color $bgColor
+</style>
